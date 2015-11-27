9910bbeb92c93e94251c40b2b70b7fc0e07e3ae2
kohzy <kohzhengyang@gmail.com>
2015-11-27 11:53:13 -0500

    Added left column buttons

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//Variables for p5 sound speech recognition
-var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
-myRec.continuous = true; // do continuous recognition
-myRec.interimResults = true; // allow partial recognition (faster, less accurate)
+// //Variables for p5 sound speech recognition
+// var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+// myRec.continuous = true; // do continuous recognition
+// myRec.interimResults = true; // allow partial recognition (faster, less accurate)
 
 
 //create a new Leaflet map and set the initial view when the map loads and the initial zoom
-var nycmap = L.map('map').setView([40.742045, -73.995328],13);
+var nycmap = L.map('map').setView([40.722045, -73.945328],13);
 
 //Set the choice of map style 
 var actualTileLayer = L.tileLayer.provider('Hydda.Base').addTo(nycmap);
@@ -56,10 +56,8 @@ var rawData = $.getJSON('https://data.cityofnewyork.us/resource/h9gi-nx95.json',
 					    );	
 					} else {			
 					}
-				}
-		}
-		for (var i=0; i< jsonData.length; i++) {
-			if(jsonData[i].borough == "QUEENS") {
+			}
+			else if (jsonData[i].borough == "QUEENS") {
 				var dotColor = 'red';
 				var numberInjured = parseInt(jsonData[i].number_of_persons_injured);
 				var collisionTime = parseFloat(jsonData[i].time);
@@ -95,10 +93,8 @@ var rawData = $.getJSON('https://data.cityofnewyork.us/resource/h9gi-nx95.json',
 					    );	
 					} else {			
 					}
-				}
-		}
-		for (var i=0; i< jsonData.length; i++) {
-			if(jsonData[i].borough == "BROOKLYN") {
+			}
+			else if(jsonData[i].borough == "BROOKLYN") {
 				var dotColor = 'red';
 				var numberInjured = parseInt(jsonData[i].number_of_persons_injured);
 				var collisionTime = parseFloat(jsonData[i].time);
@@ -134,10 +130,8 @@ var rawData = $.getJSON('https://data.cityofnewyork.us/resource/h9gi-nx95.json',
 					    );	
 					} else {			
 					}
-				}
-		}
-		for (var i=0; i< jsonData.length; i++) {
-			if(jsonData[i].borough == "BRONX") {
+			}				
+			else if(jsonData[i].borough == "BRONX") {
 				var dotColor = 'red';
 				var numberInjured = parseInt(jsonData[i].number_of_persons_injured);
 				var collisionTime = parseFloat(jsonData[i].time);
@@ -173,67 +167,134 @@ var rawData = $.getJSON('https://data.cityofnewyork.us/resource/h9gi-nx95.json',
 					    );	
 					} else {			
 					}
-				}
+			}
 		}
 });
 
 
-function setup() {
-	// instructions:
+// function setup() {
+// 	// instructions:
  
-  myRec.onResult = parseResult; // recognition callback
-  myRec.start(); // start engine
-}
+//   // myRec.onResult = parseResult; // recognition callback
+//   // myRec.start(); // start engine
+// }
 
 
 //Jquery Map Animations
-// $( "#map").mouseenter(function() {
-// 	actualTileLayer.setOpacity(0.3);
-// 	$(".leaflet-clickable").toggle("display");
-// 	$(this).mouseleave(function() {
-// 		actualTileLayer.setOpacity(1);
-// 		$(".leaflet-clickable").toggle("display");
-// 	})
-// })
+$("#manhattanButton").click(function() {
+	this.style.backgroundColor = this.style.backgroundColor == "white" ? "black" : "white";
+	this.style.color = this.style.color == "black" ? "white" : "black";
+	if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {
+		actualTileLayer.setOpacity(0.8);
+	}
+	else if ($(".manhattan-array").css("display") !== "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {	 
+		actualTileLayer.setOpacity(1);
+	}
+	$(".manhattan-array").toggle("display");
+})
+$("#brooklynButton").click(function() {
+	this.style.backgroundColor = this.style.backgroundColor == "white" ? "black" : "white";
+	this.style.color = this.style.color == "black" ? "white" : "black";
+	if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {
+		actualTileLayer.setOpacity(0.8);
+	}
+	else if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") !== "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {	 
+		actualTileLayer.setOpacity(1);
+	}
+	$(".brooklyn-array").toggle("display");
+})
+$("#bronxButton").click(function() {
+	this.style.backgroundColor = this.style.backgroundColor == "white" ? "black" : "white";
+	this.style.color = this.style.color == "black" ? "white" : "black";
+	if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {
+		actualTileLayer.setOpacity(0.8);
+	}
+	else if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") !== "none"
+		) {	 
+		actualTileLayer.setOpacity(1);
+	}
+	$(".bronx-array").toggle("display");
+})
+$("#queensButton").click(function() {
+	this.style.backgroundColor = this.style.backgroundColor == "white" ? "black" : "white";
+	this.style.color = this.style.color == "black" ? "white" : "black";
+	if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") == "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {
+		actualTileLayer.setOpacity(0.8);
+	}
+	else if ($(".manhattan-array").css("display") == "none"
+		&& $(".brooklyn-array").css("display") == "none"
+		&& $(".queens-array").css("display") !== "none"
+		&& $(".bronx-array").css("display") == "none"
+		) {	 
+		actualTileLayer.setOpacity(1);
+	}
+	$(".queens-array").toggle("display");
+})
 
-
-function parseResult()
-{
-  // recognition system will often append words into phrases.
-  // so hack here is to only use the last word:
-  var mostrecentword = myRec.resultString.split(' ').pop();
-  if(mostrecentword.indexOf("Manhattan")!==-1) {
-		actualTileLayer.setOpacity(0.3);
-		if($(".manhattan-array").css("display") == "none") {
-			$(".manhattan-array").toggle("display");
-		}
-	}
-  else if(mostrecentword.indexOf("Queens")!==-1) {
-		actualTileLayer.setOpacity(0.3);
-		if($(".queens-array").css("display") == "none") {
-			$(".queens-array").toggle("display");
-		}
-	}
-  else if(mostrecentword.indexOf("Bronx")!==-1) {
-		actualTileLayer.setOpacity(0.3);
-		if($(".bronx-array").css("display") == "none") {
-			$(".bronx-array").toggle("display");
-		}
-	}
-  else if(mostrecentword.indexOf("Brooklyn")!==-1) {
-		actualTileLayer.setOpacity(0.3);
-		if($(".brooklyn-array").css("display") == "none") {
-			$(".brooklyn-array").toggle("display");
-		}
-	}
-  else if(mostrecentword.indexOf("clear")!==-1) {
-  		actualTileLayer.setOpacity(1);
- 		if($(".leaflet-clickable").css("display") != "none") {
-			$(".leaflet-clickable").toggle("display");
-		}
-	}
-  console.log(mostrecentword);
-}
+// function parseResult()
+// {
+//   // recognition system will often append words into phrases.
+//   // so hack here is to only use the last word:
+//   var mostrecentword = myRec.resultString.split(' ').pop();
+//   if(mostrecentword.indexOf("Manhattan")!==-1) {
+// 		actualTileLayer.setOpacity(0.3);
+// 		if($(".manhattan-array").css("display") == "none") {
+// 			$(".manhattan-array").toggle("display");
+// 		}
+// 	}
+//   else if(mostrecentword.indexOf("Queens")!==-1) {
+// 		actualTileLayer.setOpacity(0.3);
+// 		if($(".queens-array").css("display") == "none") {
+// 			$(".queens-array").toggle("display");
+// 		}
+// 	}
+//   else if(mostrecentword.indexOf("Bronx")!==-1) {
+// 		actualTileLayer.setOpacity(0.3);
+// 		if($(".bronx-array").css("display") == "none") {
+// 			$(".bronx-array").toggle("display");
+// 		}
+// 	}
+//   else if(mostrecentword.indexOf("Brooklyn")!==-1) {
+// 		actualTileLayer.setOpacity(0.3);
+// 		if($(".brooklyn-array").css("display") == "none") {
+// 			$(".brooklyn-array").toggle("display");
+// 		}
+// 	}
+//   else if(mostrecentword.indexOf("clear")!==-1) {
+//   		actualTileLayer.setOpacity(1);
+//  		if($(".leaflet-clickable").css("display") != "none") {
+// 			$(".leaflet-clickable").toggle("display");
+// 		}
+// 	}
+//   console.log(mostrecentword);
+// }
 
 
 
